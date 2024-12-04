21b4b50a159edcc11e77e7f606092ef1a8aa9704
Daganep <Daganep@yandex.ru>
2024-12-04 20:33:04 +0300

    init commit

---
+++ app/src/main/java/com/openkin/bookstoreapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.openkin.bookstoreapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.openkin.bookstoreapp.ui.theme.BookStoreAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fs = Firebase.firestore
+//        fs.collection("books")
+//            .document()
+//            .set()
+        enableEdgeToEdge()
+        setContent {
+            BookStoreAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BookStoreAppTheme {
+        Greeting("Android")
+    }
+}
